a570ae0f15bacf68639fe9ce50249350c39d0f65
Gautham Suresh <gauthamsuresh456@gmail.com>
2026-04-27 13:00:32 +0200

    Create ci cd pipline + exception and logger files created

--- loader.py
+++ loader.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 load_dotenv()
 
@@ -8,6 +9,18 @@ loader = PyPDFLoader("test.pdf")
 pages = loader.load()
 
 #inspect what you got 
+#To inspect we are gonna just take one page
 print(f"Number of pages: {len(pages)}")
 print(f"Contents from the first Page: \n {pages[0].page_content[:500]}")
 print(f"Metadata:{pages[0].metadata}")
+
+splitter = RecursiveCharacterTextSplitter(
+    chunk_size=500,  #Max characters per chunk
+    chunk_overlap=50,  #Overlap between chunk so context isnt lost
+    separators=["\n\n","\n","."," "]  #split on this order
+)
+chunks = splitter.split_documents(pages)
+
+print(f"Number of chunks: {len(chunks)}")
+print(f"Contents from the first Page: \n {chunks[0].page_content }") 
+print(f"Contents from the second Page: \n {chunks[1].page_content }") 
--- main.py
+++ main.py
@@ -2,8 +2,12 @@ from dotenv import load_dotenv
 import os
 from langchain_openai import ChatOpenAI
 
+#Loading the .env with the api key
 load_dotenv()
+
+#Loading the model we want, cus load_dotenv the model will automatically get the api key
 llm = ChatOpenAI(model="gpt-3.5-turbo",temperature=0)
 
-response = llm.invoke("What is retrival-augmentation in one sentence")
+#sending in the prompt and receiving the response back 
+response = llm.invoke("What is retrieval-augmentation in one sentence")
 print(response.content)
